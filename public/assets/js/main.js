fe862409fb7ac412ddb088bbdf2b7bba07286db6
rhyngoh <ngoh.ryan@gmail.com>
2018-08-14 19:56:07 -0500

    added sign-in and chat box working

---
+++ public/assets/js/main.js
@@ -0,0 +1,228 @@
+var db = firebase.database();
+
+// Optional sign-in using google
+function signIn() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+}
+
+// Sign out of firebase
+function signOut() {
+  firebase.auth().signOut();
+}
+
+function signInAnon() {
+  firebase.auth().signInAnonymously()
+  .catch(function(error) {
+    console.error('Error signing in anonymously', error);
+  })
+}
+// Initiate firebase auth
+function initFirebaseAuth() {
+  firebase.auth().onAuthStateChanged(authStateObserver);
+}
+// return profile pic
+function getProfilePicUrl() {
+  return firebase.auth().currentUser.photoURL || '/public/assets/images/profile_placeholder.png';
+}
+
+// return signed in users name
+function getUserName() {
+  return firebase.auth().currentUser.displayName;
+}
+
+// check if user is signed-in
+function isUserSignedIn() {
+  return !!firebase.auth().currentUser;
+}
+
+// load chat message and listen for new messages
+function loadMessages() {
+  var callback = function(snap) {
+    var data = snap.val();
+    displayMessage(snap.key, data.name, data.text, data.profilePicUrl, data.imageUrl);
+  };
+
+  db.ref('/messages/').limitToLast(12).on('child_added', callback);
+  db.ref('/messages/').limitToLast(12).on('child_changed', callback);
+}
+
+// Saves a new message on the Firebase DB.
+function saveMessage(messageText) {
+  return db.ref('/messages/').push({
+    name: getUserName(),
+    text: messageText,
+    profilePicUrl: getProfilePicUrl()
+  }).catch(function(error){
+    console.error('Error writing message to Firebase', error);
+  });
+}
+
+// Triggered when the send new message form is submitted.
+function onMessageFormSubmit(e) {
+  e.preventDefault();
+  // Check that the user entered a message and is signed in.
+  if (messageInputElement.value && checkSignedInWithMessage()) {
+    console.log(messageInputElement.value);
+    saveMessage(messageInputElement.value).then(function() {
+      // Clear message text field and re-enable the SEND button.
+      resetMaterialTextfield(messageInputElement);
+      toggleButton();
+    });
+  }
+}
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+function authStateObserver(user) {
+  if (user) { // User is signed in!
+    // Get the signed-in user's profile pic and name.
+    var profilePicUrl = getProfilePicUrl();
+    var userName = getUserName();
+
+    // Set the user's profile pic and name.
+    userPicElement.style.backgroundImage = 'url(' + profilePicUrl + ')';
+    userNameElement.textContent = userName;
+
+    // Show user's profile and sign-out button.
+    userNameElement.removeAttribute('hidden');
+    userPicElement.removeAttribute('hidden');
+    signOutButtonElement.removeAttribute('hidden');
+
+    // Hide sign-in button.
+    signInButtonElement.setAttribute('hidden', 'true');
+    signInButtonAnon.setAttribute('hidden', 'true');
+
+    // We save the Firebase Messaging Device token and enable notifications.
+    saveMessagingDeviceToken();
+  } else { // User is signed out!
+    // Hide user's profile and sign-out button.
+    userNameElement.setAttribute('hidden', 'true');
+    userPicElement.setAttribute('hidden', 'true');
+    signOutButtonElement.setAttribute('hidden', 'true');
+
+    // Show sign-in button.
+    signInButtonElement.removeAttribute('hidden');
+    signInButtonAnon.removeAttribute('hidden');
+  }
+}
+
+// Returns true if user is signed-in. Otherwise false and displays a message.
+function checkSignedInWithMessage() {
+  // Return true if the user is signed in Firebase
+  if (isUserSignedIn()) {
+    return true;
+  }
+
+  // Display a message to the user using a Toast.
+  var data = {
+    message: 'You must sign-in first',
+    timeout: 2000
+  };
+  signInSnackbarElement.MaterialSnackbar.showSnackbar(data);
+  return false;
+}
+
+// Resets the given MaterialTextField.
+function resetMaterialTextfield(element) {
+  element.value = '';
+  element.parentNode.MaterialTextfield.boundUpdateClassesHandler();
+}
+
+// Template for messages.
+var MESSAGE_TEMPLATE =
+    '<div class="message-container">' +
+      '<div class="spacing"><div class="pic"></div></div>' +
+      '<div class="message"></div>' +
+      '<div class="name"></div>' +
+    '</div>';
+
+// A loading image URL.
+var LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif?a';
+
+// Displays a Message in the UI.
+function displayMessage(key, name, text, picUrl, imageUrl) {
+  var div = document.getElementById(key);
+  // If an element for that message does not exists yet we create it.
+  if (!div) {
+    var container = document.createElement('div');
+    container.innerHTML = MESSAGE_TEMPLATE;
+    div = container.firstChild;
+    div.setAttribute('id', key);
+    messageListElement.appendChild(div);
+  }
+  if (picUrl) {
+    div.querySelector('.pic').style.backgroundImage = 'url(' + picUrl + ')';
+  }
+  div.querySelector('.name').textContent = name;
+  var messageElement = div.querySelector('.message');
+  if (text) { // If the message is text.
+    messageElement.textContent = text;
+    // Replace all line breaks by <br>.
+    messageElement.innerHTML = messageElement.innerHTML.replace(/\n/g, '<br>');
+  } else if (imageUrl) { // If the message is an image.
+    var image = document.createElement('img');
+    image.addEventListener('load', function() {
+      messageListElement.scrollTop = messageListElement.scrollHeight;
+    });
+    image.src = imageUrl + '&' + new Date().getTime();
+    messageElement.innerHTML = '';
+    messageElement.appendChild(image);
+  }
+  // Show the card fading-in and scroll to view the new message.
+  setTimeout(function() {div.classList.add('visible')}, 1);
+  messageListElement.scrollTop = messageListElement.scrollHeight;
+  messageInputElement.focus();
+}
+
+// Enables or disables the submit button depending on the values of the input
+// fields.
+function toggleButton() {
+  if (messageInputElement.value) {
+    submitButtonElement.removeAttribute('disabled');
+  } else {
+    submitButtonElement.setAttribute('disabled', 'true');
+  }
+}
+
+// Checks that the Firebase SDK has been correctly setup and configured.
+function checkSetup() {
+  if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
+    window.alert('You have not configured and imported the Firebase SDK. ' +
+        'Make sure you go through the codelab setup instructions and make ' +
+        'sure you are running the codelab using `firebase serve`');
+  }
+}
+
+// Checks that Firebase has been imported.
+checkSetup();
+
+// Shortcuts to DOM Elements.
+var messageListElement = document.getElementById('messages');
+var messageFormElement = document.getElementById('message-form');
+var messageInputElement = document.getElementById('message');
+var submitButtonElement = document.getElementById('submit');
+var imageButtonElement = document.getElementById('submitImage');
+// var imageFormElement = document.getElementById('image-form');
+var mediaCaptureElement = document.getElementById('mediaCapture');
+var userPicElement = document.getElementById('user-pic');
+var userNameElement = document.getElementById('user-name');
+var signInButtonElement = document.getElementById('sign-in');
+var signInButtonAnon = document.getElementById('sign-in-anonymous');
+var signOutButtonElement = document.getElementById('sign-out');
+var signInSnackbarElement = document.getElementById('must-signin-snackbar');
+
+// Saves message on form submit.
+messageFormElement.addEventListener('submit', onMessageFormSubmit);
+signOutButtonElement.addEventListener('click', signOut);
+signInButtonElement.addEventListener('click', signIn);
+signInButtonAnon.addEventListener('click', signInAnon);
+
+// Toggle for the button.
+messageInputElement.addEventListener('keyup', toggleButton);
+messageInputElement.addEventListener('change', toggleButton);
+
+// initialize Firebase
+initFirebaseAuth();
+
+// We load currently existing chat messages and listen to new ones.
+loadMessages();
